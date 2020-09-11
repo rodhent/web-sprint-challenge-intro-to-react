6d9255b907c0789175e278e2a9b5c1b702f98ee2
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-09-11 13:17:28 -0400

    mvp update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./components/Character";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [characters, setCharacters] = useState([]);
+  const URL = `https://swapi.dev/api/people/`;
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        // console.log(res.data.results);
+        setCharacters(res.data.results);
+      })
+      .catch((err) => console.log("Error", err));
+  }, []);
+
+  console.log(characters);
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character list={characters} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,46 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import App from "../App";
+import axios from "axios";
+import styled from "styled-components";
+import "../App.css";
+import cuid from "cuid";
+
+const StyledDiv = styled.div`
+  background-color: grey;
+  color: white;
+  max-width: 80%;
+  width: 400px;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px 2px;
+  margin: 2.5% auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h2 {
+    color: black;
+    transition: all 0.5s ease-in-out;
+    &:hover {
+      color: yellow;
+      transition: all 0.5s ease-in-out;
+    }
+  }
+`;
+
+const Character = ({ list }) => {
+  console.log("props info", list);
+  return (
+    <>
+      {list.map((list) => (
+        <StyledDiv key={cuid()}>
+          <h2>{list.name}</h2>
+          <p>Gender: {list.gender}</p>
+          <p>Birth Year: {list.birth_year}'</p>
+        </StyledDiv>
+      ))}
+    </>
+  );
+};
+
+export default Character;
